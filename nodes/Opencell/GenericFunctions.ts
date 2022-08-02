2c48af35b2db5ae3c19eaf8f69897fd40b9002d3
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-08-02 11:43:50 +0200

    Refactor custom fields conversion

--- nodes/Opencell/GenericFunctions.ts
+++ nodes/Opencell/GenericFunctions.ts
@@ -99,3 +99,69 @@ export async function validateCredentials(this: ICredentialTestFunctions ,decryp
 
 	return await this.helpers.request(requestOptions);
 }
+
+export function convertCustomFields(executeFunction: IExecuteFunctions, customFieldsValues : IDataObject[]) {
+	const convertedCustomFields:IDataObject[] = [];
+
+	for(const cf of customFieldsValues) {
+
+		const currentCf:IDataObject = {
+			code:cf.code,
+			fieldType: cf.fieldType,
+		};
+
+		switch(String(cf.fieldType)) {
+			case 'LIST':
+			case 'CHECKBOX_LIST':
+				//convert list values to the format expected by the api aka "value":[{"value":"VAL1"},{"value":"VAL2"}]
+				const valueField = cf.value as string[];
+				if(valueField && valueField.toString() !== '') {
+					//Multiple values case
+					if(Array.isArray(valueField)) {
+						const valueList:IDataObject[] = [];
+						for(const value of valueField) {
+							valueList.push({
+								'value':value,
+							});
+						}
+						currentCf.value = valueList;
+					}
+					//Single values case
+					else {
+						const value = cf.value;
+						currentCf.value = [{
+							'value':value,
+						}];
+					}
+				}
+				break;
+			case 'STRING':
+			case 'TEXT_AREA':
+				currentCf.stringValue = cf.stringValue;
+				break;
+			case 'DATE':
+				currentCf.dateValue = cf.dateValue;
+				break;
+			case 'BOOLEAN':
+				currentCf.booleanValue = cf.booleanValue;
+				break;
+			case 'LONG':
+				currentCf.longValue = cf.longValue;
+				break;
+			case 'DOUBLE':
+				currentCf.doubleValue = cf.doubleValue;
+				break;
+			default:
+				throw new NodeApiError(executeFunction.getNode(), {error: `Custom field type unsupported: ${cf.type}`});
+		}
+		if(cf.code) {
+			//Remove everything after | in 'code'
+			currentCf.code = String(cf.code).split('|')[0];
+		}
+
+		convertedCustomFields.push(currentCf);
+
+	}
+
+	return convertedCustomFields;
+}
